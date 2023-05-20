f3e317f9cec84889d7fea845d2ab4a0159ccbf13
Mithun Rajan <mithunrajan@gmail.com>
2023-05-20 19:49:48 -0400

    fix: types for state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ import { api } from "@/utils/api";
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
-  const [ipAddress, setIpAddress] = useState('');
+  const [ipAddress, setIpAddress] = useState<string>('');
 
   useEffect(() => {
     const getIp = async () => {
